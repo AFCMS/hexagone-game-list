8d1e42fd9c00b149566c1d5737e3b846fc411919
AFCMS <afcm.contact@gmail.com>
2025-01-30 13:13:32 +0100

    Disallow nonn-steam domains in proxy

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
 
+const allowedDomains = [
+  "https://store.steampowered.com",
+  "https://steamcommunity.com",
+];
+
 const app = express();
 
 app.use(cors());
@@ -13,6 +18,18 @@ app.get("*", async (req, res) => {
       res.status(400).send("No URL provided");
       return;
     }
+
+    const isAllowedDomain = allowedDomains.some((domain) =>
+      targetPath.startsWith(domain)
+    );
+
+    if (!isAllowedDomain) {
+      res
+        .status(400)
+        .send("URL must be from store.steampowered.com or steamcommunity.com");
+      return;
+    }
+
     const response = await axios.get(targetPath);
     res.status(response.status).send(response.data);
   } catch (error) {
